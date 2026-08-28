c409c629579b40be9336eb5f30e0294d64d07e7e
joaoemanuel4896-lang <joaoemanuel4896@gmail.com>
2026-08-28 09:17:48 -0500

    Add functions to show present and card

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirPresente() {
+    document.querySelector(".inicio").style.display = "none";
+    document.querySelector(".presente").style.display = "block";
+}
+
+function mostrarCarta() {
+    document.getElementById("carta").style.display = "block";
+}
